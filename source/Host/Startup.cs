0432d7a96045640c6c34cf48633c7f129cb20800
Brock Allen <brockallen@gmail.com>
2014-07-22 21:14:56 -0400

    move providers into own repos

---
+++ source/Host/Startup.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license
+ */
+
+using Owin;
+
+namespace Thinktecture.IdentityManager.Host
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var factory = new Thinktecture.IdentityManager.Host.AspNetIdentityIdentityManagerFactory("AspId");
+            app.UseIdentityManager(new IdentityManagerConfiguration()
+            {
+                IdentityManagerFactory = factory.Create
+            });
+        }
+    }
+}
